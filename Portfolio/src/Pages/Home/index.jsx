0409d5abf24b68866f5e32cd060bbe4afbb9c67f
eyyub bayramov <eyyubbayramov58@gmail.com>
2023-11-10 05:19:19 -0800

    home

--- Portfolio/src/Pages/Home/index.jsx
+++ Portfolio/src/Pages/Home/index.jsx
@@ -31,6 +31,8 @@ const Home=()=>{
 <Typography color="black" className="fronthome" sx={{fontFamily:" TradeGothic LT Extended"}} marginTop="30px" marginLeft="30px" variant="h3">
                Salam , Mən Bayramov Eyyub
             </Typography>
+
+
 <div data-aos="zoom-out-left">
             <Stack className="frontgit" flexDirection="row" alignItems="center" gap="10px">
 
@@ -47,17 +49,22 @@ const Home=()=>{
             </Stack>
 
 </div>
+
            </Stack>
 
 
-
+<Stack className="a" flexDirection="row" alignItems="center">
+    <Typography  marginLeft="30px" width="50%">
+    Veb saytıma xoş gəldiniz! Bu ərazidə mənə dair ən son yenilikləri, bacarıqlarımı, nailiyyətlərimi və uğurlu proyektlərimi tapa bilərsiniz. Təcrübəmi öyrənmək və daha çox məlumat əldə etmək üçün sizi mənimlə əlaqə saxlamaya çağırıram.
+    </Typography>
 <div className="le" data-aos="flip-down">
 
 
-<Box  width="50%" boxShadow=" 0px 50px 100px -20px rgba(50, 50, 93, 0.25), 0px 30px 60px -30px rgba(0, 0, 0, 0.3), inset 0px -2px 6px 0px rgba(10, 37, 64, 0.35);" className="frontback" marginLeft="30%" padding='10px'  marginTop="6px">
+<Box  width="50%" boxShadow=" 0px 50px 100px -20px rgba(50, 50, 93, 0.25), 0px 30px 60px -30px rgba(0, 0, 0, 0.3), inset 0px -2px 6px 0px rgba(10, 37, 64, 0.35);" className="frontback" marginLeft="40%" padding='10px'  marginTop="6px">
     <img className="poht" style={{width:"100%"}}  src={front} alt="" />
 </Box>
 </div> 
+</Stack>
 
    
         </PageContainer>
