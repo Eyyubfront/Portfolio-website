3db31ec2dff2771f4146b2de11d99fdfb523b2d9
eyyub bayramov <eyyubbayramov58@gmail.com>
2024-07-31 06:41:12 -0700

    FINIS PORTFOLIO

--- Portfolio/src/Companents/Burgermenu/index.jsx
+++ Portfolio/src/Companents/Burgermenu/index.jsx
@@ -3,77 +3,78 @@ import MenuIcon from "@mui/icons-material/Menu";
 import { IconButton, Stack, Hidden } from "@mui/material";
 import { useState } from "react";
 
-
 const BurgerMenu = () => {
   const [open, setOpen] = useState(false);
+
+  // Menü açma ve kapama işlevi
+  const toggleMenu = () => setOpen(prev => !prev);
+
+  // Menü kapalı olduğunda setOpen fonksiyonunu çağırarak menüyü kapatır
+  const handleMenuClick = () => setOpen(false);
+
   return (
     <>
-
-      <Hidden mdUp> 
-  
+      <Hidden mdUp>
         <div className="burger-container">
           <IconButton
             className="menu-icon"
-            onClick={() => setOpen(!open)}
+            onClick={toggleMenu}
           >
-            {open ? <Close  /> : <MenuIcon />}
+            {open ? <Close /> : <MenuIcon />}
           </IconButton>
           {open && (
             <div className={`menu-drawer ${open ? 'open' : ''}`}>
-              <div data-aos="fade-down"  data-aos-easing="linear"
-     data-aos-duration="300">
-              <Stack flexDirection="row" justifyContent="flex-end">
-                <IconButton
-                  className="close-icon"
-                  edge="end"
-                  onClick={() => setOpen(false)}
-                >
-                  <Close className="cloceicon" />
-                </IconButton>
-              </Stack>
-              <div className="navbar__container">
-                <div className="navbar__top">
-                  <div className="navbar__hrefs">
-                    <a href="#about">
-                      <div className="navbarhref">
-                        <h3 className="burgermenunames">About</h3>
-                      </div>
-                    </a>
-                    <a href="#skils">
-                      <div className="navbarhref">
-                        <h3 className="burgermenunames">Skills</h3>
-                      </div>
-                    </a>
-                    <a href="#experience">
-                      <div className="navbarhref">
-                        <h3 className="burgermenunames">Experience</h3>
-                      </div>
-                    </a>
-                    <a href="#projects">
-                      <div className="navbarhref">
-                        <h3 className="burgermenunames">Projects</h3>
-                      </div>
-                    </a>
-                    <a href="#contact">
-                      <div className="navbarhref">
-                        <h3 className="burgermenunames">Contact</h3>
-                      </div>
-                    </a>
-                    <a href="https://github.com/Eyyubfront">
-                      <div className="navbar__githubeyyub">
-                        Github Profile
-                      </div>
-                    </a>
+              <div data-aos="fade-down" data-aos-easing="linear" data-aos-duration="300">
+                <Stack flexDirection="row" justifyContent="flex-end">
+                  <IconButton
+                    className="close-icon"
+                    edge="end"
+                    onClick={() => setOpen(false)}
+                  >
+                    <Close className="cloceicon" />
+                  </IconButton>
+                </Stack>
+                <div className="navbar__container">
+                  <div className="navbar__top">
+                    <div className="navbar__hrefs">
+                      <a href="#about" onClick={handleMenuClick}>
+                        <div className="navbarhref">
+                          <h3 className="burgermenunames">About</h3>
+                        </div>
+                      </a>
+                      <a href="#skils" onClick={handleMenuClick}>
+                        <div className="navbarhref">
+                          <h3 className="burgermenunames">Skills</h3>
+                        </div>
+                      </a>
+                      <a href="#experience" onClick={handleMenuClick}>
+                        <div className="navbarhref">
+                          <h3 className="burgermenunames">Experience</h3>
+                        </div>
+                      </a>
+                      <a href="#projects" onClick={handleMenuClick}>
+                        <div className="navbarhref">
+                          <h3 className="burgermenunames">Projects</h3>
+                        </div>
+                      </a>
+                      <a href="#contact" onClick={handleMenuClick}>
+                        <div className="navbarhref">
+                          <h3 className="burgermenunames">Contact</h3>
+                        </div>
+                      </a>
+                      <a href="https://github.com/Eyyubfront" onClick={handleMenuClick}>
+                        <div className="navbar__githubeyyub">
+                          Github Profile
+                        </div>
+                      </a>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
           )}
-          </div>
- 
+        </div>
       </Hidden>
-    
     </>
   );
 };
